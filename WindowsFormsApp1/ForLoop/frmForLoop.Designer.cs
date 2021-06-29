93de06dd31d35947689a65d692b661000513d5da
Manel Alonso <macalonspc@gmail.com>
2021-06-29 12:36:58 +0200

    for loop implementation finished. closes #3

--- WindowsFormsApp1/ForLoop/frmForLoop.Designer.cs
+++ WindowsFormsApp1/ForLoop/frmForLoop.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtStudentId = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtStudentId
+            // 
+            this.txtStudentId.Location = new System.Drawing.Point(279, 113);
+            this.txtStudentId.Name = "txtStudentId";
+            this.txtStudentId.Size = new System.Drawing.Size(242, 20);
+            this.txtStudentId.TabIndex = 9;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(279, 196);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(242, 20);
+            this.txtSurname.TabIndex = 8;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(279, 237);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(242, 20);
+            this.txtAge.TabIndex = 7;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(279, 156);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(242, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Location = new System.Drawing.Point(312, 308);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(160, 29);
+            this.BtnSave.TabIndex = 5;
+            this.BtnSave.Text = "Save";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // frmForLoop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtStudentId);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.BtnSave);
+            this.Name = "frmForLoop";
             this.Text = "frmForLoop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtStudentId;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button BtnSave;
     }
 }
--- WindowsFormsApp1/ForLoop/frmForLoop.cs
+++ WindowsFormsApp1/ForLoop/frmForLoop.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyFirstApp.ForeachLoop;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,33 @@ namespace MyFirstApp.ForLoop
         public frmForLoop()
         {
             InitializeComponent();
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            Student student = new Student();
+            student.StudentId = Convert.ToInt32(txtStudentId.Text);
+            student.Name = txtName.Text;
+            student.Surname = txtSurname.Text;
+            student.Age = Convert.ToInt32(txtAge.Text);
+
+            List<Student> studentList = new List<Student>();
+            //Boxing, estas poniendo un objeto dentro de una "caja"
+            studentList.Add(student);
+
+            for (int i = 0; i < studentList.Count; i++)
+            {
+                //Unboxing, estas sacando un objeto de dentro de una "caja"
+                MessageBox.Show(studentList[i].Name + " " + studentList[i].Surname);
+            }
+
+            /* misma función pero casteando el objeto automaticamente dentro de la declaración del Foreach
+            foreach (Student _student in studentList)
+            {
+                //Unboxing, estas sacando un objeto de dentro de una "caja"
+                MessageBox.Show(_student.Name + " " + _student.Surname;
+            }
+            */
         }
     }
 }

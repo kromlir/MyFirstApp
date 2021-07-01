72a907cd2c6b6b1d80cc503a0c0da998666b2953
Manel Alonso <macalonspc@gmail.com>
2021-07-01 14:13:05 +0200

    implementation of abstract classes. closes #11

---
+++ MyFirstApp/AbstractClases/GeeksForGeeks.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp.AbstractClases
+{    
+    public abstract class GeeksForGeeks : ICloneable
+    {
+        /*
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+        */
+        //abstract method 'gfg()'
+        public abstract void gfg();
+
+        public abstract object Clone(); 
+
+        public string Test()
+        {
+            return "we are testing";
+        }
+
+        public virtual string VirtualMethod()
+        {
+            return "we are in virtualMethod";
+        }
+    }
+}

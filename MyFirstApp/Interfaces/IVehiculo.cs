a38ccd0e61e9fe9176a079c0040d7621373a7231
Manel Alonso <macalonspc@gmail.com>
2021-07-02 10:40:43 +0200

    Interface implementation with abstract methods. closes #13

---
+++ MyFirstApp/Interfaces/IVehiculo.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFirstApp.Interfaces
+{
+    interface IVehiculo
+    {
+        void Acelerar(int km);
+        void Frenar();
+        void Girar(int angulos);
+        void Despegar();
+        void Aterrizar();
+
+    }
+}
---
+++ MyFirstApp/Interfaces/frmInterfaces.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstApp.Interfaces
+{
+    public partial class frmInterfaces : Form
+    {
+        public frmInterfaces()
+        {
+            InitializeComponent();
+        }
+
+        private void frmInterfaces_Load(object sender, EventArgs e)
+        {
+            //interface polymorphism
+            IVehiculo vehiculo = new Avion();
+
+            //Aqui no podría acceder al metodo clone
+            vehiculo.Acelerar(1000);
+
+            //interface polymorhpism
+            ICloneable clonable = new Avion();
+            clonable.Clone();
+        }
+    }
+}
